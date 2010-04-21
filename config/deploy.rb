7a80d4f615e093a341ef2d43c7c70d55f0413a52
mifan <mifanxp@gmail.com>
2010-04-21 20:54:53 +0800

    add cap for deploy

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+set :application, "railsjam.com"
+set :repository,  "git://github.com/mifan/rails_jam.git"
+
+
+set :scm, :git
+set :user, "stoneworm"
+set :use_sudo, false
+
+
+set :deploy_to, "/home/#{user}/#{application}"
+
+role :app, "railsjam.com"
+ 
+
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app do
+  end
+end
